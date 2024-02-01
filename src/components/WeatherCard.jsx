b1a71b0a45c8866af516bf44667a2bd6fe5c1168
David <atembarry12@gmail.com>
2024-02-01 04:21:01 +0100

    Barebones v1 Complete

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -17,18 +17,15 @@ function WeatherCard(props){
     }
 
    return(
-      <Grid container justifySelf={"center"} spacing={1.5}>
-        <Grid item sx={12} md={4} lg={3} alignContent={"center"}>
-        <img src="/src/assets/wr1.jpg" style={{width: "100%", borderRadius: "1.25rem"}}/>
-        </Grid>
-       <Grid item sx={12} md={8} lg={9} style={{padding: "0.25rem 1rem"}}>
+      <Grid container spacing={1.5}>
+       <Grid item style={{padding: "0.25rem 1rem"}}>
       <p style={{display: "block", margin: "1.5rem"}}>
         <span className="important_text">{floor(weather_data.current.temp)}<sup>o</sup>C</span>
        <span className="important_text">{weather_data.timezone}</span> 
        <span className="important_text" style={{textTransform: "capitalize"}}>{weather_data.current.weather[0].description} </span>
        </p>
         <Grid container spacing={2} justifyItems={"center"}>
-        {weather_forecast.map((cast)=><Grid item xs={12} md={4} lg={2}>
+        {weather_forecast.map((cast)=><Grid item>
         <a href="/" className="container" key={`cook_${weather_forecast.indexOf(cast)}`}>
               <span className="temp_bold">{floor(cast.feels_like.day)} <sup>o</sup>C</span>
                 Max|Min : <span>{floor(cast.temp.max)}</span><sup>o</sup> | <span>{floor(cast.temp.min)}</span><sup>o</sup>
