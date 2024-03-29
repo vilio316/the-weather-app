5ec0444fc6758ae116508cf489c583e70336f2f9
David <atembarry12@gmail.com>
2024-03-29 19:11:44 +0100

    Weather Report, V1 Complete

--- src/components/WeatherCardByDay.jsx
+++ src/components/WeatherCardByDay.jsx
@@ -1,6 +1,36 @@
 import { useLoaderData } from "react-router-dom";
+import { makeDate, floor, toKmH, percent} from "./WeatherCard";
 
 export default function WeatherCardByDay(){
     let result = useLoaderData();
-    console.log(result)
+    console.log(result)   
+
+    const makeTime = (timestamp) => {
+        let date = new Date(timestamp * 1000)
+        return `${date.getHours()}:${date.getMinutes()}`
+    }
+
+    return(
+        <>
+        <p className="important_text">
+            {makeDate(result.dt)}
+        </p>
+        <p>
+            <span style={{padding:"0 0.25rem"}}>Day: {floor(result.temp.day)}<sup>o</sup></span>
+            <span style={{padding:"0 0.25rem"}} >Night: {floor(result.temp.night)}<sup>o</sup></span>
+        </p>
+        <span style={{fontSize: "3.5rem"}}>{floor(result.feels_like.day)}<sup>o</sup></span>
+        <p></p>
+        <div>
+            <header>
+                <h2><u>Details</u></h2>
+                <p>Wind Speed : {toKmH(result.wind_speed)}km/h</p>
+                <p>Chance of Rain: {percent(result.pop)}% </p>
+                <p>Humidity: {result.humidity}% </p>
+                <p>Sunrise/ Sunset: {makeTime(result.sunrise)} / {makeTime(result.sunset)}</p>
+                <p>Moonrise/ Moonset: {makeTime(result.moonrise)} / {makeTime(result.moonset)}</p>
+            </header>
+        </div>
+        </>
+    )
 }
